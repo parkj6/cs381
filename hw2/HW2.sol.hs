2afd63adc6778ff37ef72159ffce0fcd21d6ee84
Jong Park <parkj6@oregonstate.edu>
2019-02-02 21:28:49 -0800

    HW2 solution up

---
+++ hw2/HW2.sol.hs
@@ -0,0 +1,135 @@
+module HW2 where
+
+-- | Binary trees with nodes labeled by values of an arbitrary type.
+data Tree a
+   = Node a (Tree a) (Tree a)
+   | End
+  deriving (Eq,Show)
+
+-- | One step in a path, indicating whether to follow the left subtree (L)
+--   or the right subtree (R).
+data Step = L | R
+  deriving (Eq,Show)
+
+-- | A path is a sequence of steps. Each node in a binary tree can be
+--   identified by a path, indicating how to move down the tree starting
+--   from the root.
+type Path = [Step]
+
+-- | Create a leaf node.
+leaf :: a -> Tree a
+leaf x = Node x End End
+
+-- | An example tree.
+ex :: Tree Int
+ex = Node 4 (Node 3 (leaf 2) End)
+            (Node 7 (Node 5 End (leaf 6))
+                    (leaf 8))
+
+
+-- | Map a function over a tree. Applies the given function to every label
+--   in the tree, preserving the tree's structure.
+--   
+--   >>> mapTree odd End
+--   End
+--
+--   >>> mapTree even (Node 5 (leaf 2) End)
+--   Node False (Node True End End) End
+--
+--   >>> (mapTree not . mapTree even) (Node 5 End (leaf 2))
+--   Node True End (Node False End End)
+--
+--   >>> mapTree (+10) ex
+--   Node 14 (Node 13 (Node 12 End End) End) (Node 17 (Node 15 End (Node 16 End End)) (Node 18 End End))
+--
+--   >>> ex == (mapTree (subtract 27) . mapTree (+27)) ex
+--   True
+--
+mapTree :: (a -> b) -> Tree a -> Tree b
+mapTree f End          = End
+mapTree f (Node x l r) = Node (f x) (mapTree f l) (mapTree f r)
+
+
+-- | Get the value at the node specified by a path. Returns 'Nothing' if
+--   the given path is invalid.
+--
+--   >>> valueAt [] ex
+--   Just 4
+--
+--   >>> valueAt [L,L] ex
+--   Just 2
+--
+--   >>> valueAt [L,R] ex
+--   Nothing
+--
+--   >>> valueAt [R,L,R] ex
+--   Just 6
+--
+--   >>> valueAt [L,L,L] ex
+--   Nothing
+--
+valueAt :: Path -> Tree a -> Maybe a
+valueAt _     End          = Nothing
+valueAt []    (Node x _ _) = Just x
+valueAt (L:p) (Node _ l _) = valueAt p l
+valueAt (R:p) (Node _ _ r) = valueAt p r
+
+
+-- | Find a path to a node that contains the given value.
+--
+--   >>> pathTo 3 (leaf 5)
+--   Nothing
+--
+--   >>> pathTo 5 ex
+--   Just [R,L]
+--
+--   >>> pathTo 6 ex
+--   Just [R,L,R]
+--
+--   >>> pathTo 4 ex
+--   Just []
+--
+--   >>> pathTo 10 ex
+--   Nothing
+--
+pathTo :: Eq a => a -> Tree a -> Maybe Path
+pathTo x End = Nothing
+pathTo x (Node y l r)
+  | x == y    = Just []
+  | otherwise = case pathTo x l of
+                  Just p  -> Just (L:p)
+                  Nothing -> case pathTo x r of
+                               Just p  -> Just (R:p)
+                               Nothing -> Nothing
+
+
+-- Here is a slightly nicer implementation that groups the two
+-- recursive calls using a pair. Note that because of lazy
+-- evaluation, there is no performance penalty for doing this!
+-- If the value is found in the left branch, the subexpression
+-- 'pathTo x r' will never be evaluated.
+--
+-- pathTo x End  = Nothing
+-- pathTo x (Node y l r)
+--   | x == y    = Just []
+--   | otherwise = case (pathTo x l, pathTo x r) of
+--                   (Just p, _) -> Just (L:p)
+--                   (_, Just p) -> Just (R:p)
+--                   _           -> Nothing
+
+
+-- And here is an even nicer implementation using a feature
+-- called "pattern guards". The basic idea is that a guard
+-- of the form '<pattern> <- <expr>' will succeed if the
+-- result of the expression matches the pattern and fail
+-- otherwise (and then the next guard will be tried). This
+-- is not a feature you need to know since you can always
+-- just use case-expressions instead, but it often leads
+-- to cleaner looking code.
+--
+-- pathTo x End = Nothing
+-- pathTo x (Node y l r)
+--   | x == y               = Just []
+--   | Just p <- pathTo x l = Just (L:p)
+--   | Just p <- pathTo x r = Just (R:p)
+-- pathTo _ _               = Nothing
