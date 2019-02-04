974a8b0e33a7c44012c592139945ee340bce7464
Jong Park <parkj6@oregonstate.edu>
2019-02-03 21:19:37 -0800

    I finished hw1

---
+++ hw1/HW1.parkj6.hs
@@ -0,0 +1,207 @@
+module HW1 where
+
+
+-- | Integer-labeled binary trees.
+data Tree
+   = Node Int Tree Tree   -- ^ Internal nodes
+   | Leaf Int             -- ^ Leaf nodes
+  deriving (Eq,Show)
+
+
+-- | An example binary tree, which will be used in tests.
+t1 :: Tree
+t1 = Node 1 (Node 2 (Node 3 (Leaf 4) (Leaf 5))
+                    (Leaf 6))
+            (Node 7 (Leaf 8) (Leaf 9))
+
+-- | Another example binary tree, used in tests.
+t2 :: Tree
+t2 = Node 6 (Node 2 (Leaf 1) (Node 4 (Leaf 3) (Leaf 5)))
+            (Node 8 (Leaf 7) (Leaf 9))
+
+
+-- | The integer at the left-most node of a binary tree.
+--
+--   >>> leftmost (Leaf 3)
+--   3
+--
+--   >>> leftmost (Node 5 (Leaf 6) (Leaf 7))
+--   6
+--
+--   >>> leftmost t1
+--   4
+--
+--   >>> leftmost t2
+--   1
+--
+leftmost :: Tree -> Int
+leftmost (Leaf n)    = n
+leftmost (Node _ l _) = leftmost l
+
+
+
+
+-- | The integer at the right-most node of a binary tree.
+--
+--   >>> rightmost (Leaf 3)
+--   3
+--
+--   >>> rightmost (Node 5 (Leaf 6) (Leaf 7))
+--   7
+--
+--   >>> rightmost t1
+--   9
+--
+--   >>> rightmost t2
+--   9
+--
+rightmost :: Tree -> Int
+rightmost (Leaf n)    = n
+rightmost (Node _ _ r) = rightmost r
+
+-- | Get the maximum integer from a binary tree.
+--
+--   >>> maxInt (Leaf 3)
+--   3
+--
+--   >>> maxInt (Node 5 (Leaf 4) (Leaf 2))
+--   5
+--
+--   >>> maxInt (Node 5 (Leaf 7) (Leaf 2))
+--   7
+--
+--   >>> maxInt t1
+--   9
+--
+--   >>> maxInt t2
+--   9
+--
+maxInt :: Tree -> Int
+maxInt (Leaf n)     =  n
+maxInt (Node n l r) | n > maxInt l && n > maxInt r = n
+                    | n < maxInt l = maxInt l
+                    | n < maxInt r = maxInt r
+-- maxInt (Node n l r) = max n (max (maxInt l) (maxInt r))
+
+-- | Get the minimum integer from a binary tree.
+--
+--   >>> minInt (Leaf 3)
+--   3
+--
+--   >>> minInt (Node 2 (Leaf 5) (Leaf 4))
+--   2
+--
+--   >>> minInt (Node 5 (Leaf 4) (Leaf 7))
+--   4
+--
+--   >>> minInt t1
+--   1
+--
+--   >>> minInt t2
+--   1
+--
+minInt :: Tree -> Int
+minInt (Leaf n)     =  n
+minInt (Node n l r) = min n (min (minInt l) (minInt r))
+
+
+-- | Get the sum of the integers in a binary tree.
+--
+--   >>> sumInts (Leaf 3)
+--   3
+--
+--   >>> sumInts (Node 2 (Leaf 5) (Leaf 4))
+--   11
+--
+--   >>> sumInts t1
+--   45
+--
+--   >>> sumInts (Node 10 t1 t2)
+--   100
+--
+sumInt :: Tree -> Int
+sumInt (Leaf n) = n
+sumInt (Node n l r) = n + sumInt l + sumInt r
+
+
+-- | The list of integers encountered by a pre-order traversal of the tree.
+--
+--   >>> preorder (Leaf 3)
+--   [3]
+--
+--   >>> preorder (Node 5 (Leaf 6) (Leaf 7))
+--   [5,6,7]
+--
+--   >>> preorder t1
+--   [1,2,3,4,5,6,7,8,9]
+--
+--   >>> preorder t2
+--   [6,2,1,4,3,5,8,7,9]
+--
+preorder :: Tree -> [Int]
+preorder (Leaf n) = [n]
+preorder (Node n l r) = n : preorder l ++ preorder r
+
+
+-- | The list of integers encountered by an in-order traversal of the tree.
+--
+--   >>> inorder (Leaf 3)
+--   [3]
+--
+--   >>> inorder (Node 5 (Leaf 6) (Leaf 7))
+--   [6,5,7]
+--
+--   >>> inorder t1
+--   [4,3,5,2,6,1,8,7,9]
+--
+--   >>> inorder t2
+--   [1,2,3,4,5,6,7,8,9]
+--
+inorder :: Tree -> [Int]
+inorder (Leaf n)     = [n]
+inorder (Node n l r) = inorder l ++ n:[] ++ inorder r
+
+
+-- | Check whether a binary tree is a binary search tree.
+--
+--   >>> isBST (Leaf 3)
+--   True
+--
+--   >>> isBST (Node 5 (Leaf 6) (Leaf 7))
+--   False
+--
+--   >>> isBST t1
+--   False
+--
+--   >>> isBST t2
+--   True
+--
+isBST :: Tree -> Bool
+isBST (Leaf n)     = True
+isBST (Node n l r) = maxInt l < n
+                   && minInt r > n
+                   && isBST l && isBST r
+
+-- | Check whether a number is contained in a binary search tree.
+--   (You may assume that the given tree is a binary search tree.)
+--
+--   >>> inBST 2 (Node 5 (Leaf 2) (Leaf 7))
+--   True
+--
+--   >>> inBST 3 (Node 5 (Leaf 2) (Leaf 7))
+--   False
+--
+--   >>> inBST 4 t2
+--   True
+--
+--   >>> inBST 10 t2
+--   False
+--
+inBST :: Int -> Tree -> Bool
+inBST x (Leaf n)     | x == n = True
+                     | otherwise = False
+inBST x (Node n l r) | x == n = True
+                     | x < n = inBST x l
+                     | x > n = inBST x r
+--                     | x < minInt l = False
+--                     | x > maxInt r = False
--- quiz1.hs
+++ quiz1.hs
@@ -2,17 +2,27 @@ module Quiz1 where
 
 -- import Prelude hiding (toInt, even, and, (.))
 
-data Result = OK Int 
-			| Error
+data Result = OK Int
+      | Error
 
 toInt :: Result -> Int
 toInt Error = -1
 toInt (OK x) = x
- 
+
 -- even :: Int -> Bool
 
 add :: Int -> Int -> Int
 add x y = x + y
 
--- (.) :: (b -> c) -> (a -> b) -> a -> c
+sub :: Int -> Int -> Int
+sub x y = x - y
 
+pos :: Int -> Int -> Bool
+pos x y | x-y > 0 = True
+        | otherwise = False
+
+test :: Int -> Bool -> Int
+test x True = x
+test x False = -127
+
+-- (.) :: (b -> c) -> (a -> b) -> a -> c
