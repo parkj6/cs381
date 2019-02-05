0c12ee4b36e81c9d29f75b2a449f5c71ea1ea8ba
Lyons <lyonsja@oregonstate.edu>
2019-02-04 20:40:12 -0800

    Type, data, and two macro definitions completed

--- hw3/HW3.parkj6.hs
+++ hw3/HW3.parkj6.hs
@@ -2,33 +2,34 @@
 
 module HW3 where
 
--- | A functional programming language that creates a MinLogo language
+import Prelude hiding (Num)
+-- | A functional programming language that creates a MiniLogo language
 -- | that moves pens around a cartesian plane.
-
 -- | Syntax of MiniLogo is defined by following Grammar.
-
 -- prog	::=	ε   |   cmd ; prog	sequence of commands
+
 type Prog = [Cmd]
 
 -- num	::=	(any natural number)
-data Num = Int
-  deriving (Eq,Show)
+type Num = Int
+
 -- var	::=	(any variable name)
-data Var = variable
-  deriving (Eq,Show)
+type Var = String
+
 -- macro	::=	(any macro name)
-data Macro = Prog
-  deriving (Eq,Show)
+type Macro = String
+
 
 -- cmd	::=	pen mode	change pen mode
 -- |	move ( expr , expr )	move pen to a new position
 -- |	define macro ( var* ) { prog }  	define a macro
 -- |	call macro ( expr* )	invoke a macro
 data Cmd
-    = Pen Mode          -- change pen mode
-    | Move Expr Expr    -- Move pen
-    | Macro [Var] Prog  -- Define a macro
-    | Call Macro [Expr] -- Invoke a macro
+    = Pen Mode                 -- change pen mode
+    | Move Expr Expr           -- Move pen
+    | Define Macro [Var] Prog  -- Define a macro
+    | Call Macro [Expr]        -- Invoke a macro
+    deriving(Eq, Show)
 
 -- mode	::=	down   |   up	pen status
 data Mode = Up | Down
@@ -37,6 +38,40 @@ data Mode = Up | Down
 -- expr	::=	var	variable reference
 -- |	num	literal number
 -- |	expr + expr	addition expression
-data Expr = Int
-          | And Expr Expr
+data Expr = VAR Var
+          | NUM Num
+          | EXPR Expr Expr
   deriving (Eq, Show)
+
+
+--define line (x1,y1,x2,y2) {
+--  pen up; move (x1,y1); pen down; move (x2,y2);
+--}
+
+
+line = Define "line" ["x1","y1","x2","y2"][Pen Up, (Move (VAR "x1") (VAR "y1")), Pen Down, (Move (VAR "x2") (VAR "y2")) ]
+
+--define nix (x, y, h, w){
+-- call line(x,y, x+w, y+h)
+-- call line(x+w, y, x, y+h)
+--}
+
+nix = Define "nix"["x", "y", "h", "w"][Call "line" [VAR "x", VAR "y", EXPR (VAR "x") (VAR "w"), EXPR (VAR "y") (VAR "h")], 
+                                       Call "line" [EXPR(VAR "x")(VAR "w"), VAR "y", VAR "x", EXPR (VAR "y") (VAR "h")]]
+
+
+
+
+--steps :: Int -> Prog
+
+
+
+
+
+--macros :: Prog-> [Macro]
+
+
+
+
+
+--pretty :: Prog -> String
