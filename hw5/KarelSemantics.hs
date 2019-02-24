7435f4100a3ae1d542a04bf964ff56ba5bb3a627
Jong Park <parkj6@oregonstate.edu>
2019-02-24 01:11:47 -0800

    stmt can PutBeeper now.

--- hw5/KarelSemantics.hs
+++ hw5/KarelSemantics.hs
@@ -11,11 +11,11 @@ import KarelExamples
 
 -- | Valuation function for Test.
 test :: Test -> World -> Robot -> Bool
-test (Not t)    w r        = not (test t w r) 
-test (Facing c) _ r        = c == (getFacing r)
-test (Clear d)  w r        = isClear (relativePos d r) w
-test Beeper     w r        = hasBeeper (getPos r) w
-test Empty      _ r        = isEmpty r
+test (Not t)    w r = not (test t w r) 
+test (Facing c) _ r = c == (getFacing r)
+test (Clear d)  w r = isClear (relativePos d r) w
+test Beeper     w r = hasBeeper (getPos r) w
+test Empty      _ r = isEmpty r
 
 -- | Valuation function for Stmt.
 stmt :: Stmt -> Defs -> World -> Robot -> Result
@@ -24,14 +24,18 @@ stmt PickBeeper    _ w r = let p = getPos r
                         in if hasBeeper p w
                               then OK (decBeeper p w) (incBag r)
                               else Error ("No beeper to pick at: " ++ show p)
+stmt PutBeeper     _ w r = let p = getPos r
+                        in if test (Not Empty) w r && hasBeeper p w || 
+                              test (Not Empty) w r && test (Clear Front) w r
+                              then OK (incBeeper p w) (decBag r)
+                              else Error ("No beeper to put.") 
 stmt Move          _ _ r = undefined
-stmt PutBeeper     _ _ r = undefined
 stmt (Turn d)      _ _ r = undefined
+stmt (Block (a:b)) _ _ r = undefined
+stmt (If t s1 s2)  _ _ r = undefined
 stmt (Call m)      _ _ r = undefined
 stmt (Iterate i s) _ _ r = undefined
-stmt (If t s1 s2)  _ _ r = undefined
 stmt (While t s)   _ _ r = undefined
-stmt (Block (a:b)) _ _ r = undefined
 
 -- | Run a Karel program.
 prog :: Prog -> World -> Robot -> Result
